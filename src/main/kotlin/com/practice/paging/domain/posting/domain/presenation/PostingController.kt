15df5c60a466f89bacb87050007eb1166fb143d6
jeayoung <k01066624566@gmail.com>
2022-12-09 10:42:28 +0900

    add PostingController

---
+++ src/main/kotlin/com/practice/paging/domain/posting/domain/presenation/PostingController.kt
@@ -0,0 +1,31 @@
+package com.practice.paging.domain.posting.domain.presenation
+
+import com.practice.paging.domain.posting.domain.presenation.dto.request.PostingWriteReqDto
+import com.practice.paging.domain.posting.domain.presenation.dto.response.PostingListResDto
+import com.practice.paging.domain.posting.domain.service.GetAllPostingService
+import com.practice.paging.domain.posting.domain.service.PostingWriteService
+import org.springframework.data.domain.Pageable
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/posting")
+class PostingController(
+    private val getAllPostingService: GetAllPostingService,
+    private val postingWriteService: PostingWriteService,
+){
+
+    @PostMapping
+    fun writePosting(@RequestBody postingWriteReqDto: PostingWriteReqDto): ResponseEntity<Void>{
+        postingWriteService.execute(postingWriteReqDto)
+        return ResponseEntity.ok().build()
+    }
+
+    @GetMapping
+    fun getAllPosting(pageable: Pageable): ResponseEntity<PostingListResDto> =
+        ResponseEntity.ok(getAllPostingService.execute(pageable))
+}
